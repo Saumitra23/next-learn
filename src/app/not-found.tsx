02e11108f55894ac2eca80f920b1564ec73bb032
Saumitra23 <saumitrashukla23@gmail.com>
2024-08-24 12:54:30 +0530

    adding custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-1 flex-col w-screen h-screen justify-center text-center">
+      <h1>Page not found</h1>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
--- src/app/products/[productId]/reviews/[reviewCount]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewCount]/page.tsx
@@ -1,10 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewCount({
   params,
 }: {
   params: { reviewCount: string; productId: string };
 }) {
   console.log("params", params);
-  return (
+  return parseInt(params.reviewCount) > 100 ? (
+    notFound()
+  ) : (
     <h3>
       Total reviews for Product {params.productId} is {params.reviewCount}
     </h3>
